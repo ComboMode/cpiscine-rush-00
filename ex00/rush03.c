7b8741dc93adde54f437ec13d3acc1e699cc5e2d
Gabriel Bellon <gabriel1490@gmail.com>
2022-07-02 23:36:44 +0100

    Rush 03 done

---
+++ ex00/rush03.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpadovan <jpadovan@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 23:33:34 by jpadovan          #+#    #+#             */
+/*   Updated: 2022/07/02 23:33:37 by jpadovan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	print_at(int line, int column, int max_l, int max_c)
+{
+	if (line == 0)
+	{
+		if (column == 0)
+			ft_putchar('A');
+		else if (column == max_c)
+			ft_putchar('C');
+		else
+			ft_putchar('B');
+	}
+	else if (line == max_l)
+	{
+		if (column == 0)
+			ft_putchar('A');
+		else if (column == max_c)
+			ft_putchar('C');
+		else
+			ft_putchar('B');
+	}
+	else
+	{
+		if (column == 0 || column == max_c)
+			ft_putchar('B');
+		else
+			ft_putchar(' ');
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	line;
+	int	column;
+
+	line = 0;
+	while (line < y)
+	{
+		column = 0;
+		while (column < x)
+		{
+			print_at(line, column, y - 1, x - 1);
+			column++;
+		}
+		ft_putchar('\n');
+		line++;
+	}
+}
